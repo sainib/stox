74aa67bcceae9ed9187a17b9593835d595b2f599
saini <birender.saini@gmail.com>
2018-09-15 14:59:17 -0400

    rearrange

--- prepDataFile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-fn=$1
-
-tail -n +2 $1 > $1.1
-expLineNum=`grep -ni exported $1.1 | head -1 | awk -F':' '{ print $1 }'`
-linesToFetch=$(( $expLineNum -2 )) ;
-head -n ${linesToFetch} $1.1 > $1.ready
-rm $1.1 
